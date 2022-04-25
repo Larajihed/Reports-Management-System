797543995b5c08fe590cd38492deff74f0758e63
Jihed Larayedh <Larajihade@gmail.com>
2022-04-25 09:39:09 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,13 @@ import {getFirestore} from 'firebase/firestore'
 
 
 const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: 'https://reports-generator-1b38f-default-rtdb.firebaseio.com/',
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+   apiKey: "AIzaSyAqRXaqi1HpwFY8D1Adp0Kn3XS9SI4Hcy4",
+  authDomain: "wonder-rms.firebaseapp.com",
+  projectId: "wonder-rms",
+  storageBucket: "wonder-rms.appspot.com",
+  messagingSenderId: "427603407231",
+  appId: "1:427603407231:web:3c78075bcec121e7986a4a",
+  measurementId: "G-R7XWYQ1XZ4"
 })
 
 export const auth = app.auth()
